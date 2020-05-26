9c0a08698755ed2fefe4bb340aff92ccfe6db41e
Mukhtar Haris <mukhtarharis.lethalaric@gmail.com>
2020-05-26 16:23:59 +0700

    search komiku to get manga's url

--- node/index.js
+++ node/index.js
@@ -1,5 +1,43 @@
-const mainFunc = () => {
+var cheerio = require('cheerio')
+var axios = require('axios')
+var Promise = require('bluebird')
+
+const baseURL = 'https://komiku.co.id'
+
+const title = 'black clover'
+
+const fetchPage = async(url) => {
+    try {
+        const result = await axios.get(url)
+        return result.data
+    } catch (error) {
+        console.log('error while fetching page : ', error)
+        throw error
+    }
+}
+
+const mainFunc = async() => {
     console.log('hello world!!!')
+
+
+    const finalUrl = baseURL + '?post_type=manga&s=' + title
+
+    console.log('final url : ', finalUrl)
+
+    const html = await fetchPage(finalUrl)
+        // console.log('data : ', html)
+    var $ = cheerio.load(html)
+        // console.log("$ : ", $.html())
+    const size = $('.daftar .bge').map((i, elem) => {
+            console.log('log : ', elem.children[1].attribs)
+        })
+        // console.log(' size list : ', size)
 };
 
 mainFunc()
+    .then(datas => {
+
+
+    }).catch(error => {
+        console.log('error : ', error)
+    })
