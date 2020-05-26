ee1906512b0890d07ee449ce6a18026706610ba2
Mukhtar Haris <mukhtarharis.lethalaric@gmail.com>
2020-05-26 14:56:22 +0700

    init java project

---
+++ java/src/main/java/com/mukhtar/webscrape/komiku/KomikuApplication.java
@@ -0,0 +1,13 @@
+package com.mukhtar.webscrape.komiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomikuApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/mukhtar/webscrape/komiku/KomikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukhtar.webscrape.komiku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
