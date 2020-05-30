b1828c2fdd27457dce9a586d135a38f986a9201e
Mukhtar Haris <mukhtarharis.lethalaric@gmail.com>
2020-05-30 20:04:49 +0700

    get each chapter download url

--- node/index.js
+++ node/index.js
@@ -1,6 +1,7 @@
 var cheerio = require('cheerio')
 var axios = require('axios')
 var Promise = require('bluebird')
+var fs = require('fs')
 
 const baseURL = 'https://komiku.co.id'
 
@@ -16,9 +17,17 @@ const fetchPage = async(url) => {
     }
 }
 
-const mainFunc = async() => {
-    console.log('hello world!!!')
+const fetchPage2 = async(url) => {
 
+    return axios({
+        'url': url,
+        method: 'get',
+        timeout: 10000,
+    }).then(res => res.data).catch(err => console.log(err))
+
+}
+
+const getTitleUrl = async(title) => {
 
     const finalUrl = baseURL + '?post_type=manga&s=' + title
 
@@ -28,9 +37,93 @@ const mainFunc = async() => {
         // console.log('data : ', html)
     var $ = cheerio.load(html)
         // console.log("$ : ", $.html())
-    const size = $('.daftar .bge').map((i, elem) => {
-            console.log('log : ', elem.children[1].attribs)
+
+    const titleUrlList = await $('.daftar .bge').map(async(i, elem) => {
+        console.log('log : ', elem.children[1].attribs.href)
+        console.log('logzzz : ', elem.children[1].children[3].children[1])
+
+        const value = await getChapter(elem.children[1].attribs.href)
+
+        value.splice(0, 1)
+        console.log('value : ', value)
+
+        jsonToFile(value, 'solution-' + elem.children[1].attribs.href.replace('//', '') + '.json')
+
+
+        return value
+    }).get()
+
+    return titleUrlList
+}
+
+const toJsonObject = async(result) => {
+
+    const data = await Promise.all(result)
+    console.log('datasss : ', data)
+
+    return data
+}
+
+const jsonToFile = async(result, filename) => {
+    try {
+        // var jsonData = []
+        // await toJsonObject(result).then(o => {
+        //     jsonData = o
+        // })
+
+        const jsonData = await toJsonObject(result)
+
+        // const trueJson = {}
+
+        // jsonData.forEach(data => {
+        //     Object.keys(data).forEach(key => {
+        //         trueJson[key] = data[key]
+        //     })
+        // })
+
+        fs.writeFile(filename, jsonData, (error) => {
+            if (error) {
+                console.log('error : ', error)
+            }
         })
+    } catch (error) {
+        throw error
+    }
+}
+
+const getChapter = async(url) => {
+    console.log('processed title url : ', url)
+
+    const html = await fetchPage(url)
+
+
+    // console.log('html : ', html)
+
+    var $ = cheerio.load(html)
+
+    // const downloadChapter = await $('tbody tr').map((i, elem) => {
+    //     console.log('table ==> ', elem.children[5])
+    // })
+
+    const chapter = await $('.chapter tbody tr').map(async(i, elems) => {
+        try {
+            return elems.children[5].children[1].attribs.href
+        } catch (error) {
+            return elems.children[1]
+        }
+
+    }).get()
+
+    return chapter
+
+
+}
+
+const mainFunc = async() => {
+    console.log('hello world!!!')
+
+    const url = await getTitleUrl(title)
+    console.log('list url : ', url)
         // console.log(' size list : ', size)
 };
 
